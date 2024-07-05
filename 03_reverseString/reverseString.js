fecc8cbf33cad74f87a84b2e771facef6fbda8d9
maximilianovalle <109977231+maximilianovalle@users.noreply.github.com>
2024-07-05 15:56:37 -0500

    Resolve problem 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(givenString) {
+    let length = givenString.length;
+    let reversed = "";
 
+    for (let i = length - 1; i >= 0; i--) {
+        reversed += givenString.charAt(i);
+    }
+
+    return reversed;
 };
 
 // Do not edit below this line
