13b6112480d235b84d67e5d1cccc29fe57c06676
maximilianovalle <109977231+maximilianovalle@users.noreply.github.com>
2024-07-05 15:50:18 -0500

    Resolve problem 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,1 +1,14 @@
-func
+const repeatString = function(givenString, numTimes) {
+    if (numTimes < 0) {
+        return "ERROR";
+    }
+
+    let output = "";
+    for (let i = 0; i < numTimes; i++) {
+        output += givenString;
+    }
+    return output;
+};
+
+// Do not edit below this line
+module.exports = repeatString;
