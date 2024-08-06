25340552f64ee2f8bade7f8d3db1c3f2ebf0858d
maximilianovalle <109977231+maximilianovalle@users.noreply.github.com>
2024-08-06 15:37:05 -0500

    Resolve problem 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let divisibleBy4 = false;
+    let divisibleBy100 = false;
+    let divisibleBy400 = false;
 
+    if (year % 4 == 0) divisibleBy4 = true;
+    if (year % 100 == 0) divisibleBy100 = true;
+    if (year % 400 == 0) divisibleBy400 = true;
+
+    if (!divisibleBy4) return false;
+    else if (!divisibleBy100) return true;
+    else if (!divisibleBy400) return false;
+    else return true;
+
+    // not divisible by 4 = false
+    // divisible by 4, not divisible by 100 = true
+    // divisible by 4, divisible by 100, not divisible by 400 = false
+    // divisible by 4, divisible by 100, divisible by 400 = true
 };
 
 // Do not edit below this line
