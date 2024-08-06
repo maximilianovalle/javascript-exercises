98e94a52a84a5173a4425ebd16d2f0bcd0dfb3f9
maximilianovalle <109977231+maximilianovalle@users.noreply.github.com>
2024-08-06 14:53:10 -0500

    Resolve problem 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArr, ...elementsToRemove) {
+    const GIVEN_ARR_LENGTH = givenArr.length;
+    const REMOVE_ARR_LENGTH = elementsToRemove.length;
+
+    const newArr = [];
+    let newArrCounter = 0;
+
+    for (let givenElement = 0; givenElement < GIVEN_ARR_LENGTH; givenElement++) {
+
+        outer: for (let removeElement = 0; removeElement < REMOVE_ARR_LENGTH; removeElement++) {
+
+            if (elementsToRemove[removeElement] === givenArr[givenElement]) {
+                break outer;    // skip to next element of given array
+            }
+            
+            else if ((removeElement == REMOVE_ARR_LENGTH - 1) && (elementsToRemove[removeElement] != givenArr[givenElement])) {
+                newArr[newArrCounter] = givenArr[givenElement];
+                newArrCounter++;
+            }
+        }
+    }
+
+    return newArr;
 };
 
 // Do not edit below this line
