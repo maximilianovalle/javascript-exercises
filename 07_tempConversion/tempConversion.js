a280ee44e5505d4aa56b4e5c9157ed51bb6c0b87
maximilianovalle <109977231+maximilianovalle@users.noreply.github.com>
2024-08-06 16:09:57 -0500

    Resolve problem 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let celsiusValue = (fahrenheitTemp - 32)*(5/9)
+  return Math.round(celsiusValue * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let fahrenheitValue = ((9/5)*celsiusTemp) + 32;
+  return Math.round(fahrenheitValue * 10) / 10;
 };
 
 // Do not edit below this line
